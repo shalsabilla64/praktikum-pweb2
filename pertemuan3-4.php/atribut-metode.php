5fbb864c5892f38c77f1e9fb9aaf33aec6a23cda
shalsabilla64 <165291431+shalsabilla64@users.noreply.github.com>
2024-09-11 13:13:54 +0700

    Add files via upload

---
+++ pertemuan3-4.php/atribut-metode.php
@@ -0,0 +1,19 @@
+<?php
+// Menambah Atribut dan Metode
+class Buku {
+    public $judul;
+    public $penulis;
+
+    public function __construct($judul, $penulis) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+    }
+
+    public function tampilkanInfo() {
+        return "Buku: $this->judul, ditulis oleh $this->penulis.";
+    }
+}
+
+$buku = new Buku("Pemrograman PHP", "John Doe");
+echo $buku1->tampilkanInfo();
+?>
---
+++ pertemuan3-4.php/kelasobjek.php
@@ -0,0 +1,23 @@
+<?php
+//Definisi Kelas
+class Mobil {
+    // Atribut atau Properties
+    public $merk;
+    public $warna;
+
+    // Constructor
+    public function __construct($merk, $warna) {
+        $this->merk = $merk;
+        $this->warna = $warna;
+    }
+
+    // Metode atau Function
+    public function deskripsi() {
+        return "Mobil ini adalah $this->merk berwarna $this->warna.";
+    }
+}
+
+    // Instansi Objek
+    $mobil1 = new Mobil("Toyota", "Hitam");
+    echo $mobil1->deskripsi();
+    ?>
